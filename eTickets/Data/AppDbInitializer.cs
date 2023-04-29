c4e45cd61b8cd9a6328e40a23ce6fe5c50846bd8
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2023-04-30 02:07:17 +0430

    added AppDbInitializer

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder) {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                //refrence to appdbcontext file
+                var conetext = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                //make sure db is created and exist
+                conetext.Database.EnsureCreated();
+
+                //cinema
+                //actor
+                //producer
+                //movie
+                //actors and movies
+            }
+        }
+    }
+}
