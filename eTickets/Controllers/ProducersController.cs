8f997e783b09402f7044ef49d2c05230ea7f8793
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2023-05-01 17:27:13 +0430

    added producersControllers

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,24 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+
+        //inject appdbcontext to constructor
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var AllProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
