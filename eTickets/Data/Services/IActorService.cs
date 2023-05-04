d0bfc2fd74d9824793368f86e7937b2b491cd92a
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2023-05-04 20:03:58 +0430

    added IActorService Interface

---
+++ eTickets/Data/Services/IActorService.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorService
+    {
+        IEnumerable<Actor> GetALL();
+
+        //a method to return a single Actor
+        Actor GetById(int id);
+
+        //a method to add data to db
+        void Add(Actor actor);
+
+        //update db
+        Actor Update(int id, Actor newActor);
+
+        //delete a record from db
+        void Delete(int id);
+    }
+}
