8e47b835398e80f21e0f331ddf61bebe0c9241e0
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2023-05-11 17:35:53 +0430

    EntityBaseRepository

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetALL()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T:class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetALL();
+
+        //a method to return a single T
+        Task<T> GetByIdAsync(int id);
+
+        //a method to add data to db
+        Task AddAsync(T entity);
+
+        //update db
+        Task<T> UpdateAsync(int id, T entity);
+
+        //delete a record from db
+        Task Delete(int id);
+    }
+}
