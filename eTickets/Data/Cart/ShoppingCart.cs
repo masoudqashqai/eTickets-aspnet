029ee11d9624e11e60e71659166326c2ce9a1e83
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2023-05-30 19:09:36 +0430

    shoppingcart class

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        //ctor
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).
+                Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+            return total;
+        }
+    }
+}
